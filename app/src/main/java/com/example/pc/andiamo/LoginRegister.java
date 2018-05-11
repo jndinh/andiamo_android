a79f48ce1fd0f0a977b00a6f93ad3b38fa7653fa
eliaseraphim <elia.deppe7@gmail.com>
2018-05-11 19:32:45 -0400

    LoginRegister Update
    CHANGES:
     - Constants.java
       - added PASS_LENGTH_MIN = 4 and PASS_LENGTH_MAX = 16
     - LoginRegister.java
       - seperated UI variables based on their respective layouts
       - getLoginInfo() now implemented
       - invalidLoginInput --> Displays to the user that their input is invalid via delayed posts.
     - login_register_dialog.xml
       - added id to a TextView that displays "You are not currently logged in." normally. ID is txt_loginStatus
     - elia_strings.xml
       - added strings invalid_input1 and invalid_input2

--- app/src/main/java/com/example/pc/andiamo/LoginRegister.java
+++ app/src/main/java/com/example/pc/andiamo/LoginRegister.java
@@ -10,24 +10,28 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.util.Patterns;
+import android.graphics.Color;
 
 public class LoginRegister extends DialogFragment implements View.OnClickListener {
-
-    //UI Variables
-    EditText eTxtUsername, eTxtPassword, eTxtFirstName, eTxtLastName, eTxtStreet, eTxtApt,
+    //UI Variables login_register_dialog.xml
+    private EditText eTxtUsername, eTxtPassword, eTxtFirstName, eTxtLastName, eTxtStreet, eTxtApt,
             eTxtCity, eTxtState, eTxtZip, eTxtEmail, eTxtConfirmEmail, eTxtCreatePassword,
             eTxtConfirmPassword;
 
-    TextView txtName, txtEmail, txtAddress;
+    private Button btnLogin, btnCreateAccount;
 
-    Button btnLogin, btnCreateAccount;
+    private TextView txtLoginStatus;
+
+    //UI variables user_info.xml
+    private TextView txtName, txtEmail, txtAddress;
 
     //Data Variables
-    String username, password, firstName, lastName, street, apt, city, state, zip, email,
+    private String username, password, firstName, lastName, street, apt, city, state, zip, email,
             confirmEmail, createPassword, confirmPassword;
 
-    boolean loggedIn;
-    String name, address;
+    private boolean loggedIn;
+    private String name, address;
 
 
     @Nullable
@@ -61,6 +65,8 @@ public class LoginRegister extends DialogFragment implements View.OnClickListene
     //          wrong. If there is an issue then initialize the UI in onCreateView
     private void initializeUI(View view, boolean loggedIn) {
         if(!loggedIn) {
+            txtLoginStatus = view.findViewById(R.id.txt_loginStatus);
+
             eTxtUsername = view.findViewById(R.id.eTxt_username);
             eTxtPassword = view.findViewById(R.id.eTxt_password);
 
@@ -146,17 +152,76 @@ public class LoginRegister extends DialogFragment implements View.OnClickListene
     //startLogin
     //  begins the login process
     private void startLogin() {
-        getLoginInfo();
+        int RETURN_CODE = getLoginInfo();
+        switch (RETURN_CODE) {
+            case 0:
+                //input is sanitized, login
+                break;
+            case 1:
+                //input is not sanitized, display error
+                invalidLoginInput();
+                break;
+        }
     }
 
-    private void getLoginInfo() {
-        boolean validInput = false;
+    //getLoginInfo
+    //  gets the login info from eTxtUsername and eTxtPassword
+    //  RETURN CODES:
+    //      | 0 = VALID INPUT | 1 = INVALID INPUT |
+    private int getLoginInfo() {
+        int validInput;
 
         username = eTxtUsername.getText().toString();
         password = eTxtPassword.getText().toString();
 
-        Log.d("dialogInterface", "\n\nUsername: " + username +
-                "\nPassword: " + password);
+        //sanitize input
+        if(username.length() != 0) {
+            boolean validEmail = Patterns.EMAIL_ADDRESS.matcher(username).matches();
+
+            if(validEmail) {
+                if(password.length() > Constants.PASS_LENGTH_MIN &&
+                        password.length() < Constants.PASS_LENGTH_MAX &&
+                        password.matches("[A-Za-z0-9]+")) {
+                    validInput = 0;
+                }
+                else {
+                    validInput = 1;
+                }
+            }
+            else {
+                validInput = 1;
+            }
+        }
+        else {
+            validInput = 1;
+        }
+
+        return validInput;
+    }
+
+    //invalidLoginInput
+    //  displays to the user, that their input is invalid
+    private void invalidLoginInput() {
+        txtLoginStatus.post(new Runnable() {
+            @Override
+            public void run() {
+                txtLoginStatus.setText(R.string.invalid_input1);
+                txtLoginStatus.setTextColor(Color.RED);
+            }
+        });
+        txtLoginStatus.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txtLoginStatus.setText(R.string.invalid_input2);
+            }
+        }, 2500);
+        txtLoginStatus.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                txtLoginStatus.setText(R.string.not_logged_in);
+                txtLoginStatus.setTextColor(Color.BLACK);
+            }
+        }, 5000);
     }
 
     //startCreateAccount
